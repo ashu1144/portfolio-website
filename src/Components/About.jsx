eaf4b5524b2e1115bdd8d5524ee0678e5cb1f314
skmdashan@gmail.com <ashanshaik987@gmail.com>
2025-07-03 16:44:08 +0530

    cv added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -46,7 +46,7 @@ export const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href=""
+            href="https://drive.google.com/uc?export=download&id=18MP-wnADAPqJ3537QKwaCeuj88oAwZPR"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -74,7 +74,7 @@ export const About = () => {
             <img
               //   src={profileImage}
               src={img1}
-              alt="Tarun Kaushik"
+              alt="Ashan"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>

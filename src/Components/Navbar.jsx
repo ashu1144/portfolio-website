0fd07683b86b9d4847b173787400dc40ef04444a
skmdashan@gmail.com <ashanshaik987@gmail.com>
2025-07-14 20:25:11 +0530

    slide update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
       }`}
     >
       <div className="flex items-center w-[90%] m-auto justify-between p-3  text-white ">
-        <div className="text-2xl font-semibold cursor-pointer">
+        <div className="text-2xl font-semibold cursor-pointer z-20">
           <span className="text-[#8245ec]">&lt;</span>
           <span className="text-white">Ashan</span>
           <span className="text-[#8245ec]">/</span>
@@ -83,7 +83,7 @@ export const Navbar = () => {
         </div>
 
         {/* mobile */}
-        <div className="md:hidden flex flex-row gap-3 justify-center items-center">
+        <div className="md:hidden flex flex-row gap-3 justify-center items-center z-20">
           <a href="http://www.linkedin.com/in/shaik-mohammad-ashan-b7a864322">
             <FaLinkedin className="scale-160  hover:opacity-50  transition-all duration-600 hover:rotate-360 mr-1 " />
           </a>
@@ -96,11 +96,11 @@ export const Navbar = () => {
       </div>
 
       <div
-        className={`transition-opacity duration-1000 ease-in-out absolute bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md w-full 
+        className={`transition-all duration-1000 ease-in-out absolute top-0 bg-[#050414] bg-opacity-10 backdrop-blur-md shadow-md w-full pt-15 z-0
     ${
       isOpen
-        ? "opacity-100 pointer-events-auto"
-        : "opacity-0 pointer-events-none"
+        ? "h-100 opacity-100"
+        : "h-0 overflow-hidden opacity-0 "
     }`}
       >
         <div className="flex flex-col items-center text-white ">
